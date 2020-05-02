9418668278310ebbc2285e171bfdd1d9ea023144
gabrielmougard <gabriel.mougard@gmail.com>
2020-05-02 23:10:20 +0200

    add remote graph exporter

---
+++ gtfs-core/src/main/java/com/algoadvcd/gtfs/core/nio/BucketConfiguration.java
@@ -0,0 +1,5 @@
+package com.algoadvcd.gtfs.core.nio;
+
+public class BucketConfiguration {
+	public final static String BUCKET_NAME = "gtfs-bucket"; 
+}
--- gtfs-core/src/main/java/com/algoadvcd/gtfs/core/nio/json/JSONGraphImporter.java
+++ gtfs-core/src/main/java/com/algoadvcd/gtfs/core/nio/json/JSONGraphImporter.java
@@ -1,11 +1,17 @@
 package com.algoadvcd.gtfs.core.nio.json;
 
 import java.io.*;
-import java.util.*;
+import java.nio.ByteBuffer;
 
 import com.algoadvcd.gtfs.core.Graph;
+import com.algoadvcd.gtfs.core.nio.BucketConfiguration;
 import com.algoadvcd.gtfs.core.nio.GraphImporter;
 
+import com.google.cloud.ReadChannel;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 
 public class JSONGraphImporter<GTFSVertex> implements GraphImporter<GTFSVertex> {
 		
@@ -31,14 +37,34 @@ public class JSONGraphImporter<GTFSVertex> implements GraphImporter<GTFSVertex> 
 	 */
 	public void importGraphRemote(Graph<GTFSVertex> g, String target) {
 		//bucket connection
+		// TODO : set the environment variable 
+		// ex : export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/[FILE_NAME].json"
 		
-		/*
-		 * Blob blob = bucket.get("some-file");
-		 * ReadChannel reader = blob.reader();
-		 * InputStream inputStream = Channels.newInputStream(reader);
-		 */
+		Storage storage = StorageOptions.getDefaultInstance().getService();
+		String bucketName = BucketConfiguration.BUCKET_NAME;
+		Blob blob = storage.get(BlobId.of(bucketName, target));
+		 
+		ReadChannel reader = blob.reader();
+		byte[] decoded = null;
+        ByteBuffer bytes = ByteBuffer.allocate(64 * 1024);
+        try {
+        	while (reader.read(bytes) > 0) {
+        		bytes.flip();
+			    decoded = bytes.array();
+			    bytes.clear();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        InputStream decodedStream = new ByteArrayInputStream(decoded);
+        JsonObjectIterator iterator = new JsonObjectIterator(decodedStream);
 		
-		return;
+        while(iterator.hasNext()) {
+			// TODO
+			//add to the graph
+		}
+		 
 	}
 	
 }
